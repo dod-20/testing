7fdd53b2ec084cd3701267ea30d5be863fe14e9d
dod-20 <danielodonovan20@gmail.com>
2025-09-06 18:07:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("service-worker.js")
+      navigator.serviceWorker.register("service-worker.js",  { scope: './' })
         .then(() => console.log("Service Worker registered"))
         .catch(err => console.error("Service Worker failed:", err));
+
     }
